27e86b3cac30d6225f397175122caee3917d2a77
c0d4 <c8o1d2a@gmail.com>
2026-08-01 00:36:14 +0800

    Format source files with Biome

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -10,56 +10,64 @@ export const friendLinks: FriendLink[] = [
 	// Add a friend by copying this example and removing the leading // markers:
 	{
 		name: "Shiori",
-	 	url: "https://sh10rl.top/",
-	 	description: "Shiori's Coffee Nook",
-	 	avatar: "https://vidar.club/assets/images/src/images/avatar/Shiori.b8bfe4e0.png",
+		url: "https://sh10rl.top/",
+		description: "Shiori's Coffee Nook",
+		avatar:
+			"https://vidar.club/assets/images/src/images/avatar/Shiori.b8bfe4e0.png",
 	},
 	{
 		name: "z221x",
-	 	url: "https://www.z221x.website/",
-	 	description: "z221x-Blog",
-	 	avatar: "https://vidar.club/assets/images/src/images/avatar/z221x.dc1d32d8.png",
+		url: "https://www.z221x.website/",
+		description: "z221x-Blog",
+		avatar:
+			"https://vidar.club/assets/images/src/images/avatar/z221x.dc1d32d8.png",
 	},
 	{
 		name: "Rencj",
-	 	url: "https://blog.rencj.top/",
-	 	description: "Rencj’s Blog",
-	 	avatar: "https://vidar.club/assets/images/src/images/avatar/Rencj.c97c04fd.jpg",
+		url: "https://blog.rencj.top/",
+		description: "Rencj’s Blog",
+		avatar:
+			"https://vidar.club/assets/images/src/images/avatar/Rencj.c97c04fd.jpg",
 	},
 	{
 		name: "ScaredCube",
-	 	url: "https://sccube.link/",
-	 	description: "ScaredCube’s Site",
-	 	avatar: "https://vidar.club/assets/images/src/images/avatar/scaredcube.23d87dc5.jpg",
+		url: "https://sccube.link/",
+		description: "ScaredCube’s Site",
+		avatar:
+			"https://vidar.club/assets/images/src/images/avatar/scaredcube.23d87dc5.jpg",
 	},
 	{
 		name: "NaCl",
-	 	url: "https://blog.naclwww.com/",
-	 	description: "NaCl Blog",
-	 	avatar: "https://vidar.club/assets/images/src/images/avatar/NaCl.347873a7.png",
+		url: "https://blog.naclwww.com/",
+		description: "NaCl Blog",
+		avatar:
+			"https://vidar.club/assets/images/src/images/avatar/NaCl.347873a7.png",
 	},
 	{
 		name: "woshiluo",
-	 	url: "https://woshiluo.com/",
-	 	description: "Woshiluo's Notebook",
-	 	avatar: "https://vidar.club/assets/images/src/images/avatar/woshiluo.32aef4b1.png",
+		url: "https://woshiluo.com/",
+		description: "Woshiluo's Notebook",
+		avatar:
+			"https://vidar.club/assets/images/src/images/avatar/woshiluo.32aef4b1.png",
 	},
 	{
 		name: "柏喵Sakura",
-	 	url: "https://baimeow.cn/",
-	 	description: "bs’ realm",
-	 	avatar: "https://thirdqq.qlogo.cn/g?b=sdk&k=NWicTpzaicZibPBH6DibfBnogQ&kti=ZI090wAAAAA&s=640",
+		url: "https://baimeow.cn/",
+		description: "bs’ realm",
+		avatar:
+			"https://thirdqq.qlogo.cn/g?b=sdk&k=NWicTpzaicZibPBH6DibfBnogQ&kti=ZI090wAAAAA&s=640",
 	},
 	{
 		name: "JYC",
-	 	url: "https://www.jyc217.com/",
-	 	description: "一个人的碎碎念",
-	 	avatar: "https://www.jyc217.com/assets/Sauroncat-CZ7otR4q.jpg",
+		url: "https://www.jyc217.com/",
+		description: "一个人的碎碎念",
+		avatar: "https://www.jyc217.com/assets/Sauroncat-CZ7otR4q.jpg",
 	},
 	{
 		name: "wuliwa",
-	 	url: "https://wuli-wa.github.io/",
-	 	description: "wuli-wa?",
-	 	avatar: "https://vidar.club/assets/images/src/images/avatar/wuliwa.23b692a6.jpg",
+		url: "https://wuli-wa.github.io/",
+		description: "wuli-wa?",
+		avatar:
+			"https://vidar.club/assets/images/src/images/avatar/wuliwa.23b692a6.jpg",
 	},
 ];
